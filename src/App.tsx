fcba2ca1fa0f40958fcfab9a2a18f24b9b556347
[SW5] 이윤지 <dbswl342@gmail.com>
2023-08-13 21:58:06 +0900

    초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      안녕하세요
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/newpage/newpage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewPage = () => {
+
+};
+
+export default NewPage;
