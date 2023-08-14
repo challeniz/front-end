9f0079db8b4c41b4b3c1a80a9d6f54ba21012032
[SW5] 이윤지 <dbswl342@gmail.com>
2023-08-14 12:32:56 +0900

    newpage 커밋

---
+++ src/components/white_box_title.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const WhiteTitleBlock = styled.div`
+  padding:35px 40px;
+  border-bottom:1px solid #d9d9d9;
+
+  .h2 {
+    font-weight:500;
+    font-size: 18px;
+  }
+`
+
+interface WhiteTitleProps {
+  children: ReactNode;
+}
+
+const WhiteBoxTitle: React.FC<WhiteTitleProps> = ({ children }) => {
+  return <WhiteTitleBlock><h2>{children}</h2></WhiteTitleBlock>
+};
+
+export default WhiteBoxTitle;
