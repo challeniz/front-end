043080a2e1444ed162df14446288175bb240c0f4
Parkjunesung <junesung7979@gmail.com>
2023-08-14 14:43:32 +0900

    초기값 설정

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import './App.css';
 // pages
 import ListPage from './pages/list_page';
 import NewPage from './pages/new_page';
+import LoginPage from './pages/login/login_page';
 
 function App() {
   return (
     <Routes>
-        <Route path="/newpage" element={<NewPage />} />
-        <Route path="/listpage" element={<ListPage />} />
+      <Route path="/newpage" element={<NewPage />} />
+      <Route path="/listpage" element={<ListPage />} />
+      <Route path="/loginpage" element={<LoginPage />} />
     </Routes>
   );
 }
---
+++ src/pages/login/login_page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import Wrapper from '../../components/common/wrapper';
+
+const LoginPage = () => {
+  return (
+    <Wrapper>
+      <div>dsdsd</div>
+    </Wrapper>
+  )
+};
+
+export default LoginPage;
