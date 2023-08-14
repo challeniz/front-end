936a79df823b051fb7c48614d3ae8b74f8a13c70
Parkjunesung <junesung7979@gmail.com>
2023-08-14 20:55:00 +0900

    로그인페이지 ui구현중

--- src/pages/login/login_page.tsx
+++ src/pages/login/login_page.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import Wrapper from '../../components/common/wrapper';
 
+const Memvership = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+`
+const SocialStart = styled.p`
+  font-size: 18px;
+  margin-top: 20px;
+`
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #ccc;
+  margin-top: 50px;
+`
+
+const KakaoImage = styled.img`
+  margin-top: 20px;
+  
+`
+
 const LoginPage = () => {
   return (
     <Wrapper>
-      <div>dsdsd</div>
+      <Memvership>회원가입하기</Memvership>
+      <SocialStart>소셜 로그인으로 시작 및 가입할 수 있습니다.</SocialStart>
+      <Divider></Divider>
+      <KakaoImage src={require('../../assets/icon/kakao_login_large_narrow.png')} alt="카카오로 시작하기"></KakaoImage>
     </Wrapper>
   )
 };
