5dde4898d3be2d1c82e108055e569647d9b6f434
[SW5] 김다솔 <gyeoul299@gmail.com>
2023-08-23 17:47:48 +0900

    feat.레이아웃수정

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -2,54 +2,119 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Wrapper from '../common/wrapper';
 import CommentList from './commentlist';
+import { BsTrash3 } from 'react-icons/bs';
 
 const CommentBox = () => {
   const [username] = useState('김챌린지');
-  const [comment, setComment] = useState<string>(''); // comment의 타입을 string으로 지정
-  const [feedComment, setFeedComment] = useState<string[]>([]); // feedComment의 타입을 string 배열로 지정
-  const [isValid, setIsValid] = useState<boolean>(false); // isValid의 타입을 boolean으로 지정
+  const [comment, setComment] = useState<string>('');
+  const [feedComment, setFeedComment] = useState<string[]>([]);
+  const [isValid, setIsValid] = useState<boolean>(false);
 
   const post = () => {
     if (comment.length === 0) {
-      return; // 댓글이 비어있을 때 게시하지 않도록 합니다.
+      return;
     }
-
+    // 댓글등록하는
     const copyFeedComment = [...feedComment];
     copyFeedComment.push(comment);
     setFeedComment(copyFeedComment);
     setComment('');
   };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && isValid) {
+      post(); // 엔터 키를 누르고 유효한 경우에 댓글 등록 함수 호출
+    }
+  };
 
+  // 댓글삭제
+  const removeComment = (indexToRemove: number) => {
+    const updatedFeedComment = feedComment.filter(
+      (_, index) => index !== indexToRemove
+    );
+    setFeedComment(updatedFeedComment);
+  };
 
-  
   return (
     <div>
       <Wrapper>
-        <input
+        <H2Styled>후기,댓글</H2Styled>
+        <CommentInput
           type="text"
-          className="commentInput"
+          className="CommentInput"
           placeholder="댓글을 달아보세요!"
+          onKeyDown={handleKeyDown} // 엔터 키 감지 이벤트 핸들러 추가
           onChange={(e) => {
             setComment(e.target.value);
             setIsValid(e.target.value.length > 0);
           }}
           value={comment}
+          key="comment-input"
         />
-        <button
-          type="button"
-          className={isValid ? 'submitCommentActive' : 'submitCommentInactive'}
-          onClick={post}
-          disabled={!isValid}
-        >
-          게시
-        </button>
+        <SubmitBtn isValid={isValid} onClick={post} disabled={!isValid}>
+          입력
+        </SubmitBtn>
 
         {feedComment.map((commentArr, i) => (
-          <CommentList userName={username} userComment={commentArr} key={i} />
+          <CommentContainer key={i}>
+            <CommentList userName={username} userComment={commentArr} />
+            <RemoveBtn onClick={() => removeComment(i)}>
+              <BsTrash3 />
+            </RemoveBtn>
+          </CommentContainer>
         ))}
       </Wrapper>
     </div>
   );
 };
+
+const CommentInput = styled.input`
+  padding: 10px;
+  border: none;
+  text-align: left;
+  font-size: 14px;
+  outline: none;
+  width: 60vw;
+  float: left;
+  background-color: transparent;
+  border-bottom: 1px solid #000;
+  margin-left: 30px;
+  margin-top: 10px;
+  ::placeholder {
+    color: #999;
+  }
+`;
+const H2Styled = styled.h2`
+  font-size: 25px;
+  padding-bottom: 25px;
+  font-weight: 700;
+  text-align: left;
+  padding: 30px;
+`;
+
+const SubmitBtn = styled.button<{ isValid: boolean }>`
+  float: left;
+  position: relative;
+  left: -58px;
+  font-size: 14px;
+  width: 66px;
+  height: 34px;
+  border-radius: 10px;
+  background: #d9d9d9;
+  color: #111;
+  border: none;
+  cursor: pointer;
+`;
+const CommentContainer = styled.div`
+  padding: 0px;
+  float: left;
+`;
+
+const RemoveBtn = styled.button`
+  background-color: transparent;
+  color: #AF0F0F;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
 
 export default CommentBox;
--- src/components/comment/commentlist.tsx
+++ src/components/comment/commentlist.tsx
@@ -3,21 +3,47 @@ import styled from 'styled-components';
 import Wrapper from '../common/wrapper';
 
 interface CommentListProps {
-    userName: string;
-    userComment: string;
-  }
-  
-  const CommentList: React.FC<CommentListProps> = (props) => {
-    return (
-      <div>
-        <Wrapper>
-          <div className='userCommentBox'>
-              <p className='userName'>{props.userName}</p>
-              <div className='userComment'>{props.userComment}</div>
-          </div>
-        </Wrapper>
-      </div>
-    );
-  };
-  
+  userName: string;
+  userComment: string;
+}
+
+const CommentList: React.FC<CommentListProps> = (props) => {
+  return (
+    <div>
+      <CommentListBox>
+        <UserCommentBox>
+          <UserName>{props.userName}</UserName>
+          <UserComment >{props.userComment}</UserComment>
+        </UserCommentBox>
+      </CommentListBox>
+    </div>
+  );
+};
+
+const CommentListBox = styled.div`
+  padding: 0px;
+  margin: 0 auto;
+  text-align: left;
+`;
+
+const UserCommentBox = styled.div`
+  float:left;
+  margin-left: 40px;
+  margin-top: 10px;
+  width: 60vw;
+}
+  `;
+
+const UserName = styled.p`
+font-weight: 800;
+font-size: 16px;
+color: #444;
+margin-bottom: 4px;
+`
+const UserComment = styled.div`
+font-size: 14px;
+color: #555;
+`
+
+
 export default CommentList;
