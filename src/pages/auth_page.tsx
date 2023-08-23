b4ad54065630ba371927a2f0de70e87069386643
[SW5] 김상종 <sangjong37@gmail.com>
2023-08-23 21:01:17 +0900

    이미지 삽입 버튼 기능 추가

--- src/pages/auth_page.tsx
+++ src/pages/auth_page.tsx
@@ -14,6 +14,40 @@ const PageBack = styled.div`
   background-color: #f4f4f4;
 `;
 
+const StyledImageContainer = styled.div`
+  position: relative;
+  width: 620px;
+  height: 377px;
+  background-color: #d9d9d9;
+`;
+
+const StyledImageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const StyledLabel = styled.label`
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+`;
+
 const TextInput = styled.textarea`
   width: 620px;
   height: 377px;
@@ -35,11 +69,13 @@ const AuthWrapper = styled.div`
     border: none;
   }
 `;
+
 const H2Styled = styled.div`
   font-size: 30px;
   font-weight: 700;
   padding: 0 0 38px;
 `;
+
 const FormCancelButton = styled.button`
   cursor: pointer;
   width: 620px;
@@ -51,6 +87,7 @@ const FormCancelButton = styled.button`
   font-size: 20px;
   font-weight: 500;
 `;
+
 const FormSubmitButton = styled.button`
   cursor: pointer;
   width: 620px;
@@ -63,6 +100,7 @@ const FormSubmitButton = styled.button`
   font-size: 20px;
   font-weight: 600;
 `;
+
 const FormButton = styled.div`
   display: flex;
   justify-content: space-between;
@@ -73,8 +111,11 @@ const AuthPage: React.FC = () => {
 
 
   const [text, setText] = useState('');
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [imageButtonText, setImageButtonText] = useState("사진 넣기");
 
-  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = event.target.value;
     
     if (inputValue.length <= 70) {
@@ -82,14 +123,31 @@ const AuthPage: React.FC = () => {
     }
   };
 
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+
+    if (file) {
+      setSelectedImage(file);
+      setImageButtonText(""); // 이미지 업로드 후 버튼 내용 변경
+    }
+  };
+
+  const handleLabelClick = () => {
+    // 이미지가 삽입된 이후 다시 이미지 삽입될수 있게 클릭
+    document.getElementById("imageUpload")?.click();
+    setSelectedImage(null);
+    setImageButtonText("사진 넣기");
+  };
+
   const currentDate = new Date();
   const formattedDate = `${currentDate.getFullYear()}.${currentDate.getMonth() + 1}.${currentDate.getDate()}`;
 
-  // HTMLInputElement
 
   const handleCancelClick = () => {
     if (window.confirm("정말 취소하겠습니까?")) {
-      setText(""); // 입력한 내용을 삭제
+      setSelectedImage(null);
+      setImageButtonText("사진 넣기"); //사진 삭제
+      setText(""); // 텍스트 지우기
     }
   };
 
@@ -100,11 +158,27 @@ const AuthPage: React.FC = () => {
         <WhiteBoxContents>
           <H2Styled>{formattedDate}</H2Styled>
           <AuthWrapper>
-            <input type="file" />
+          <StyledImageContainer>
+              {selectedImage && (
+                <StyledImageWrapper>
+                  <StyledImage src={URL.createObjectURL(selectedImage)} alt="Selected" />
+                </StyledImageWrapper>
+              )}
+              <StyledLabel htmlFor="imageUpload" onClick={handleLabelClick}>
+                {imageButtonText}
+              </StyledLabel>
+              <input
+                type="file"
+                accept="image/*"
+                id="imageUpload"
+                onChange={handleImageChange}
+                style={{ width: '100%', height: '100%', opacity: 0, position: 'absolute', zIndex: -1 }}
+              />
+            </StyledImageContainer>
             <TextInput
             placeholder="70자 이내로 입력해주세요."
             value={text}
-            // onChange={handleTextChange}
+            onChange={handleTextChange}
             ></TextInput>
           </AuthWrapper>
           <FormButton>
