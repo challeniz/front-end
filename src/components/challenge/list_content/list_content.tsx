9b5b9ad11e0a239946e5de4155385330eddfac26
[SW5] 이윤지 <dbswl342@gmail.com>
2023-08-25 01:24:17 +0900

    feat : 리스트,상세페이지 데이터 호출

--- src/components/challenge/list_content/list_content.tsx
+++ src/components/challenge/list_content/list_content.tsx
@@ -1,74 +1,121 @@
+// list_content.tsx
 import React, { useState, useEffect } from 'react';
 import * as S from './list_content.style';
 import { BsCalendarRange } from 'react-icons/bs';
 import { apiInstance } from '../../../utils/api';
+import { ROUTE } from '../../../routes';
+import { Link } from 'react-router-dom';
 
-interface ChallengeData {
+// 예시로 Challenge 타입을 정의
+interface Challenge {
+  like: boolean;
   title: string;
   start_date: string;
   end_date: string;
-  // 다른 속성들도 추가 가능
+  tag: string[];
+  id: string;
+  category: string;
 }
 
-const ListContent = () => {
-  const [like, setLike] = useState(false);
-  const [title, setTitle] = useState('');
-  const [start_date, setStartDate] = useState('');
-  const [end_date, setEndDate] = useState('');
+const menuArr = [
+  { name: '전체', content: '전체', category: '' },
+  { name: '건강', content: '건강', category: '건강' },
+  { name: '취미', content: '취미', category: '취미' },
+  { name: '식습관', content: '식습관', category: '식습관' },
+  { name: '공부', content: '공부', category: '공부' },
+  { name: '환경', content: '환경', category: '환경' },
+];
 
-  const toggleLike = async () => {
-    try {
-      // 좋아요 관련 처리를 진행
-      setLike(!like);
-    } catch (error) {
-      console.error('Error toggling like:', error);
-    }
-  };
+const ListContent = () => {
+  const [challengeList, setChallengeList] = useState<Challenge[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
 
   useEffect(() => {
-    // API를 통해 데이터 가져오기
-    apiInstance
-      .get('/challenges/') // 실제 API 엔드포인트로 수정하세요
-      .then((response) => {
-        const challengesData = response.data;
+    const fetchData = async () => {
+      try {
+        const response = await apiInstance.get('/challenges/');
+        const data = response.data;
 
-        // 데이터가 존재하는지 체크
-        if (challengesData.length > 0) {
-          // title, start_date, end_date 속성을 추출하여 새 배열 만들기
-          const extractedData = challengesData.map((item: ChallengeData) => ({
-            title: item.title,
-            start_date: new Date(item.start_date).toLocaleDateString(),
-            end_date: new Date(item.end_date).toLocaleDateString(),
+        if (data.length > 0) {
+          const challenges = data.map((challenge: any) => ({
+            like: false,
+            title: challenge.title,
+            start_date: new Date(challenge.start_date).toLocaleDateString(),
+            end_date: new Date(challenge.end_date).toLocaleDateString(),
+            tag: challenge.tag,
+            id: challenge._id,
+            category: challenge.category,
           }));
-
-          // 새 배열의 첫 번째 데이터의 속성 가져오기
-          const firstChallengeData = extractedData[0];
-
-          setTitle(firstChallengeData.title);
-          setStartDate(firstChallengeData.start_date);
-          setEndDate(firstChallengeData.end_date);
+          setChallengeList(challenges);
         }
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error('Error fetching data:', error);
-      });
-  }, []); // 마운트 시에만 호출하도록 빈 배열 전달ㄴ
+      }
+    };
+
+    // 초기 로딩 시 데이터 가져오기
+    fetchData();
+  }, []);
+
+  const toggleLike = (index: number) => {
+    setChallengeList((prevList) => {
+      const updatedList = [...prevList];
+      updatedList[index].like = !updatedList[index].like;
+      return updatedList;
+    });
+  };
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredChallengeList = selectedCategory
+    ? challengeList.filter(
+        (challenge) => challenge.category === selectedCategory
+      )
+    : challengeList;
 
   return (
-    <S.ContentWrap>
-      <S.ImgStyled>
-        <img src="" alt={`Challenge`} />
-        <S.StyledHeartButton like={like} onClick={toggleLike} />
-      </S.ImgStyled>
-      <S.TabWrap>
-        <S.TabStyled>태그</S.TabStyled>
-      </S.TabWrap>
-      <S.H3Styled>{title}</S.H3Styled>
-      <S.H4Styled>
-        <BsCalendarRange />
-        {start_date} ~ {end_date}
-      </S.H4Styled>
-    </S.ContentWrap>
+    <S.ListWrap>
+      <S.TabMenu>
+        {menuArr.map((menu, index) => (
+          <li
+            key={index}
+            className={
+              selectedCategory === menu.category ? 'submenu focused' : 'submenu'
+            }
+            onClick={() => handleCategoryClick(menu.category)}
+          >
+            {menu.name}
+          </li>
+        ))}
+      </S.TabMenu>
+      <S.ContentsWrap>
+        {filteredChallengeList.map((challenge, index) => (
+          <S.ContentWrap key={index}>
+            <S.ImgStyled>
+              <img src="" alt={`Challenge`} />
+              <S.StyledHeartButton
+                like={challenge.like}
+                onClick={() => toggleLike(index)}
+              />
+            </S.ImgStyled>
+            <S.TabWrap>
+              {challenge.tag.map((tag, index) => (
+                <S.TabStyled key={index}>{tag}</S.TabStyled>
+              ))}
+            </S.TabWrap>
+            <Link to={`${ROUTE.DETAILPAGE.link}/${challenge.id}`}>
+              <S.H3Styled>{challenge.title}</S.H3Styled>
+            </Link>
+            <S.H4Styled>
+              <BsCalendarRange />
+              {challenge.start_date} ~ {challenge.end_date}
+            </S.H4Styled>
+          </S.ContentWrap>
+        ))}
+      </S.ContentsWrap>
+    </S.ListWrap>
   );
 };
 
