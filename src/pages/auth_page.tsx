7aa3f35fce6f7dedf4f63460673c49520f06b692
[SW5] 김상종 <sangjong37@gmail.com>
2023-08-24 17:33:14 +0900

    api 구현전까지 기능 완료.

--- src/pages/auth_page.tsx
+++ src/pages/auth_page.tsx
@@ -7,6 +7,7 @@ import WhiteBox from '../components/form/white_box';
 import WhiteBoxTitle from '../components/form/white_box_title';
 import WhiteBoxContents from '../components/form/white_box_contents';
 import {ROUTE} from "../routes"
+import axios from 'axios';
 
 import { useNavigate,Link } from 'react-router-dom';
 
@@ -132,6 +133,26 @@ const AuthPage: React.FC = () => {
     }
   };
 
+// api 나오면 연결하기!!!
+  // const joinHandler = async () => {
+  //   try {
+  //     const response = await axios.post('URL', {
+  //       id,
+  //       image,
+  //       text,
+  //     });
+  //     if(response.status.201) {
+  //       실행코드 alert('등록에 성공했스비다.')
+  //     실행과 동시에 내인증현황 html 뿌려주는 기능도 코드 적어야함
+  //          navigator('/detailpage')
+  //     } 
+  //   } catch (error) {
+  //     console.error('로그인 에러:', error);
+  //     alert('로그인에 실패하였습니다.');
+  //   }
+  // }
+
+
   const handleLabelClick = () => {
     // 이미지가 삽입된 이후 다시 이미지 삽입될수 있게 클릭
     document.getElementById("imageUpload")?.click();
@@ -159,22 +180,27 @@ const AuthPage: React.FC = () => {
           <H2Styled>{formattedDate}</H2Styled>
           <AuthWrapper>
           <StyledImageContainer>
-              {selectedImage && (
-                <StyledImageWrapper>
-                  <StyledImage src={URL.createObjectURL(selectedImage)} alt="Selected" />
-                </StyledImageWrapper>
-              )}
-              <StyledLabel htmlFor="imageUpload" onClick={handleLabelClick}>
-                {imageButtonText}
-              </StyledLabel>
-              <input
-                type="file"
-                accept="image/*"
-                id="imageUpload"
-                onChange={handleImageChange}
-                style={{ width: '100%', height: '100%', opacity: 0, position: 'absolute', zIndex: -1 }}
-              />
-            </StyledImageContainer>
+  {selectedImage && (
+    <StyledImageWrapper>
+      <StyledImage src={URL.createObjectURL(selectedImage)} alt="Selected" />
+    </StyledImageWrapper>
+  )}
+  <StyledLabel htmlFor="imageUpload">{imageButtonText}</StyledLabel>
+  <input
+    type="file"
+    accept="image/*"
+    id="imageUpload"
+    onChange={handleImageChange}
+    style={{
+      width: '100%',
+      height: '100%',
+      opacity: 0,
+      position: 'absolute',
+      zIndex: -1,
+      cursor: 'pointer', // add this to make the cursor indicate the input can be clicked
+    }}
+  />
+</StyledImageContainer>
             <TextInput
             placeholder="70자 이내로 입력해주세요."
             value={text}
@@ -183,7 +209,8 @@ const AuthPage: React.FC = () => {
           </AuthWrapper>
           <FormButton>
             <FormCancelButton onClick={handleCancelClick}><Link to={ROUTE.DETAILPAGE.link}>취소하기</Link></FormCancelButton>
-            <FormCancelButton><Link to={ROUTE.DETAILPAGE.link}>등록하기</Link></FormCancelButton>
+            <FormCancelButton ><Link to={ROUTE.DETAILPAGE.link}>등록하기</Link></FormCancelButton>
+            {/* <FormCancelButton onClick={joinHandler} ></FormCancelButton>  */}
           </FormButton>
         </WhiteBoxContents>
       </WhiteBox>
@@ -194,3 +221,6 @@ const AuthPage: React.FC = () => {
 };
 
 export default AuthPage;
+
+//첼린지 수정 삭제 기능 있는지 물어보기 /날짜는 그대로되 사진이나 텍스트 같은거. / 
+//달성률- 정주님께 백앤드에서 연동이 될지 아니면 내가 직접 프론트엔드에서 연동이 되게 해야 될지
