a1a373039ac8862dd86eaf8074abad87a6504eb7
[SW5] 이윤지 <dbswl342@gmail.com>
2023-09-25 16:44:50 +0900

    feat : 배지 데이터 완성

---
+++ src/components/mypage/badge/badge copy.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import * as S from './badge.style';
+import { apiInstance } from '../../../utils/api';
+
+interface Badge {
+  obtain: boolean;
+}
+
+const Badge = () => {
+  const [badges, setBadges] = useState<Badge[]>([]);
+  const [userInfo, setUserInfo] = useState({
+    id: '',
+  });
+
+  useEffect(() => {
+    async function fetchBadges() {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await apiInstance.get('/badges');
+        if (token) {
+          if (response.status === 200) {
+            console.log(response.data);
+          }
+        }
+      } catch (error) {
+        console.error('배지 정보 가져오기 오류', error);
+      }
+    }
+
+    fetchBadges();
+  }, []);
+
+  return (
+    <div>
+      <S.BadgeTitle>
+        <h3>활동 배지</h3>
+        <S.BadgeWrap>
+          <div>
+            <S.Img src="https://i.ibb.co/2d3YCQk/join.png" alt="join" />
+            <p>가입 완료</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/rtLSrH1/all.png" alt="all" />
+            <p>모든 분야 참여</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/1Gtgdfz/complete.png" alt="complete" />
+            <p>챌린지 참가</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/q75V85M/target.png" alt="target" />
+            <p>챌린지 100% 달성</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/MRLDLMN/comment.png" alt="comment" />
+            <p>받은 후기</p>
+          </div>
+        </S.BadgeWrap>
+      </S.BadgeTitle>
+
+      <S.BadgeTitle>
+        <h3>목표 배지</h3>
+        <S.BadgeWrap>
+          <div>
+            <S.Img src="https://i.ibb.co/7VtqTRY/food.png" alt="food" />
+            <p>식습관 마스터</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/wMgdVNt/eco.png" alt="eco" />
+            <p>환경 마스터</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/j5BBDCm/study.png" alt="study" />
+            <p>공부 마스터</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/2dmmDMz/artist.png" alt="artist" />
+            <p>취미 마스터</p>
+          </div>
+          <div>
+            <S.Img src="https://i.ibb.co/4K4ZFg7/muscle.png" alt="muscle" />
+            <p>건강 마스터</p>
+          </div>
+        </S.BadgeWrap>
+      </S.BadgeTitle>
+    </div>
+  );
+};
+
+export default Badge;
--- src/components/mypage/badge/badge.tsx
+++ src/components/mypage/badge/badge.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import * as S from './badge.style';
 import { apiInstance } from '../../../utils/api';
 
-interface badge {
-  obtain: Boolean;
+interface Badge {
+  name: string;
+  count: string;
+  obtain: boolean;
+  img: string;
 }
 
 const Badge = () => {
-  const [badges, setBadges] = useState([]);
+  const [badges, setBadges] = useState<Badge[]>([]);
   const [userInfo, setUserInfo] = useState({
     id: '',
   });
@@ -19,7 +22,14 @@ const Badge = () => {
         const response = await apiInstance.get('/badges');
         if (token) {
           if (response.status === 200) {
-            setBadges(response.data);
+            const badgeList = response.data.list;
+            const badgeArray: Badge[] = badgeList.map((badge: any) => ({
+              obtain: badge.obtain,
+              name: badge.name,
+              count: badge.count,
+              img: badge.img,
+            }));
+            setBadges(badgeArray);
             console.log(response.data);
           }
         }
@@ -36,52 +46,34 @@ const Badge = () => {
       <S.BadgeTitle>
         <h3>활동 배지</h3>
         <S.BadgeWrap>
-          <div>
-            <S.Img src="https://i.ibb.co/2d3YCQk/join.png" alt="join" />
-            <p>가입 완료</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/rtLSrH1/all.png" alt="all" />
-            <p>모든 분야 참여</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/1Gtgdfz/complete.png" alt="complete" />
-            <p>챌린지 참가</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/q75V85M/target.png" alt="target" />
-            <p>챌린지 100% 달성</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/MRLDLMN/comment.png" alt="comment" />
-            <p>받은 후기</p>
-          </div>
+          {badges.slice(0, 5).map((badge, index) => (
+            <div key={index}>
+              <S.Img
+                src={badge.img}
+                alt={badge.name}
+                className={`badge-image ${badge.obtain ? 'on' : ''}`}
+              />
+              <p>{badge.name}</p>
+              {index >= 2 && <h5>{badge.count}회</h5>}
+            </div>
+          ))}
         </S.BadgeWrap>
       </S.BadgeTitle>
 
       <S.BadgeTitle>
         <h3>목표 배지</h3>
         <S.BadgeWrap>
-          <div>
-            <S.Img src="https://i.ibb.co/7VtqTRY/food.png" alt="food" />
-            <p>식습관 마스터</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/wMgdVNt/eco.png" alt="eco" />
-            <p>환경 마스터</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/j5BBDCm/study.png" alt="study" />
-            <p>공부 마스터</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/2dmmDMz/artist.png" alt="artist" />
-            <p>취미 마스터</p>
-          </div>
-          <div>
-            <S.Img src="https://i.ibb.co/4K4ZFg7/muscle.png" alt="muscle" />
-            <p>건강 마스터</p>
-          </div>
+          {badges.slice(6, 10).map((badge, index) => (
+            <div key={index}>
+              <S.Img
+                src={badge.img}
+                alt={badge.name}
+                className={`badge-image ${badge.obtain ? 'on' : ''}`}
+              />
+              <p>{badge.name}</p>
+              <h5>{badge.count}회</h5>
+            </div>
+          ))}
         </S.BadgeWrap>
       </S.BadgeTitle>
     </div>
